f15bd4459858f634019382d6587f50b6787bdcac
Mamont257 <108954365+Mamont257@users.noreply.github.com>
2022-12-10 11:18:25 +0200

    почав2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ul = document.querySelector('.gallery');
+console.log(ul);
+
+
+const markup = galleryItems.reduce((acc,{ preview, original, description }) => acc + 
+`<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+ </a>`, '')
+
+// console.log(markup);
+ul.insertAdjacentHTML('beforeend', markup);
+ul.addEventListener('click', onClick);
+
+function onClick(evt) {
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+    // console.log(evt.target);
+
+    const urlOriginal = evt.target.dataset;
+
+    console.log(urlOriginal);
+
+}
+
